d77b8c103a57df3e98d905b8fb08f6819fbda4b0
hyeonw8 <whgusrud223@gmail.com>
2024-08-29 01:08:41 +0900

    add: 사용자 위치 얻어오는 함수 생성

---
+++ src/app/(public)/mate/getUserCurrentPostion.ts
@@ -0,0 +1,50 @@
+import { Position } from "@/types/position.type";
+
+interface getUserCurrentPositionProps {
+  setGeoData: (geoData: Omit<Position, "errMsg">) => void;
+  setIsUseGeo: (isUseGeo: boolean) => void;
+}
+export const getUserCurrentPosition = ({setGeoData, setIsUseGeo}: getUserCurrentPositionProps): Promise<Position | null> => {
+
+  return new Promise((resolve) => {
+    if (!navigator.geolocation) {
+      // console.error('위치 정보 사용 거부:', error);
+      const defaultPosition = {
+        center: { lat: 37.5556236021213, lng: 126.992199507869 },
+        errMsg: "Geolocation is not supported",
+        isLoading: false
+      };
+      setIsUseGeo(false);
+      setGeoData(defaultPosition);
+      resolve(defaultPosition);
+      return;
+    }
+
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const newPosition = {
+          center: {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          },
+          isLoading: false
+        };
+        setGeoData(newPosition);
+        // console.log('위치 정보 획득 성공');
+        setIsUseGeo(true);
+        resolve(newPosition);
+      },
+      (error) => {
+        // console.error('위치 정보 획득 실패:', error);
+        const defaultPosition = {
+          center: { lat: 37.5556236021213, lng: 126.992199507869 },
+          errMsg: error.message,
+          isLoading: false
+        };
+        setIsUseGeo(false);
+        setGeoData(defaultPosition);
+        resolve(defaultPosition);
+      }
+    );
+  });
+};
