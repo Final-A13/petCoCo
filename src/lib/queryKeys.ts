13efc78869b5eeaf3ce7b5af26f2106a0efbafeb
hyeonw8 <whgusrud223@gmail.com>
2024-09-02 20:18:27 +0900

    add: 쿼리 키 파일 생성

---
+++ src/lib/queryKeys.ts
@@ -0,0 +1,9 @@
+import { Json } from "@/types/supabase";
+
+export const queryKeys = {
+  // mate
+  matePosts: (id: string) => ["matePosts", id],
+  address: (center: { lat: number; lng: number }) => ["address", center],
+  geoData: ["geoData"],
+  usersPets: (petIds: Json) => ["usersPets", petIds],
+};
