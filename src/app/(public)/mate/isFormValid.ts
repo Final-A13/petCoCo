d34879abbf66abe0a667982f7244ace1dfbd3860
hyeonw8 <whgusrud223@gmail.com>
2024-09-25 01:00:19 +0900

    update: 날짜 유효성 추가
    현재보다 이전 날짜로 게시물이 등록되지 않도록, 추후 글자로 안내하거나 유효성 UI 부분 추가하기

--- src/app/(public)/mate/isFormValid.ts
+++ src/app/(public)/mate/isFormValid.ts
@@ -5,5 +5,7 @@ export const isFormValid = (formPosts: Omit<MateNextPostType, "user_id">) => {
   const { title, date_time, members, place_name, content, pet_id } = formPosts;
   // pet_id가 배열인지 확인하고, 배열일 경우에만 길이를 체크
   const isPetSelected = Array.isArray(pet_id) && pet_id.length > 0;
-  return !!(title && date_time && members && place_name && content && isPetSelected);
+  const isDateValid = date_time ? new Date(date_time) > new Date() : false;
+
+  return !!(title && isDateValid && members && place_name && content && isPetSelected);
 };
