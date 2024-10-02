6b89db5f20d19427c969534922a09e2c0e8fca4c
hyeonw8 <whgusrud223@gmail.com>
2024-10-02 18:46:57 +0900

    refactor: handleInputChange 함수로 상태 추적 변경 및 타입 수정, return 문 내부 수정

--- src/app/(public)/mate/_components/common/textAreaField.tsx
+++ src/app/(public)/mate/_components/common/textAreaField.tsx
@@ -1,25 +1,32 @@
-import { MateNextPostType } from "@/types/mate.type";
-import React, { Dispatch, SetStateAction } from "react";
-import { FormPostsType } from "./addressInfoField";
+import React from "react";
+import { FormPostsType } from "@/types/mate.type";
 
-interface TextAreaFieldProps<T extends FormPostsType> {
-  formPosts: T,
-  setFormPosts:  Dispatch<SetStateAction<T>>
+interface TextAreaFieldProps {
+  value: string;
+  formPosts: FormPostsType;
+  fieldName: keyof FormPostsType;
+  handleInputChange: (
+    fieldName: keyof FormPostsType
+  ) => (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
+  placeholder?: string;
+  className?: string;
+  maxLength?: number;
+  id: string;
 }
 
-const TextAreaField = <T extends FormPostsType>({formPosts, setFormPosts}: TextAreaFieldProps<T>) => {
+const TextAreaField = ({ formPosts, handleInputChange, value, fieldName, placeholder, className, maxLength, id}: TextAreaFieldProps) => {
   return (
     <div className="mb-[1rem] mt-[1.06rem] flex flex-col gap-y-[0.5rem] px-[1.5rem]">
       <label htmlFor="content" className="text-[1rem] font-[600]">
         내용
       </label>
       <textarea
-        value={formPosts.content || ""}
-        onChange={(e) => setFormPosts({ ...formPosts, content: e.target.value })}
-        placeholder="선호하는 산책 동선이나 총 예상 산책 시간, 혹은 특별한 요구 사항이 있다면 적어주세요."
-        className="h-[6.0625rem] w-full resize-none overflow-x-scroll rounded-[0.5rem] border border-subTitle2 p-[0.75rem] scrollbar-hide"
-        id="content"
-        maxLength={199}
+        value={value}
+        onChange={handleInputChange(fieldName)}
+        placeholder={placeholder}
+        className={className}
+        maxLength={maxLength}
+        id={id}
       ></textarea>
       <p className="flex justify-end text-subTitle2">{formPosts.content?.length}/200</p>
     </div>
--- src/app/(public)/mate/_components/common/textInputField.tsx
+++ src/app/(public)/mate/_components/common/textInputField.tsx
@@ -1,15 +1,33 @@
-import { MateNextPostType } from "@/types/mate.type";
-import React, { Dispatch, SetStateAction } from "react";
-import { FormPostsType } from "./addressInfoField";
+import { FormPostsType } from "@/types/mate.type";
+import React from "react";
 
-interface TextInputFieldProps<T extends FormPostsType> {
-  formPosts: T,
-  setFormPosts:  Dispatch<SetStateAction<T>>
+
+interface TextInputFieldProps {
+  type: string;
+  value: string;
+  formPosts: FormPostsType;
+  fieldName: keyof FormPostsType;
+  handleInputChange: (
+    fieldName: keyof FormPostsType
+  ) => (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
+  placeholder?: string;
+  className?: string;
+  min?: number;
+  id: string;
 }
-const TextInputField = <T extends FormPostsType>({formPosts, setFormPosts}: TextInputFieldProps<T>) => {
+
+const TextInputField = ({ type, value, fieldName, handleInputChange, placeholder, id, className, min}: TextInputFieldProps) => {
   return (
-    <div>TextInputField</div>
-  )
-}
+    <input
+      type={type}
+      value={value}
+      onChange={handleInputChange(fieldName)}
+      placeholder={placeholder}
+      className={className}
+      min={min}
+      id={id}
+    />
+  );
+};
 
-export default TextInputField
+export default TextInputField;
