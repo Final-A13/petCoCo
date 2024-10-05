eeecc660e2003d4bea739611f0b0e2f8faa58ca4
hyeonw8 <whgusrud223@gmail.com>
2024-10-05 20:06:01 +0900

    fix: 설명 수정 (반려동물 정보를 다 가져오던 이전과는 달리 반려견 정보만 가져오도록 route.ts 수정)

--- src/hooks/useUserPetsData.ts
+++ src/hooks/useUserPetsData.ts
@@ -10,12 +10,12 @@ import { UsersPetType } from "@/types/usersPet.type";
  * @file useUserPetsData.ts
  * @brief 사용자의 반려견에 대한 정보를 가져오는 훅
  * 
- * @returns userPets - 사용자의 반려동물에 대한 정보를 포함하는 배열, 타입 : UsersPetType[]
+ * @returns userPets - 사용자의 반려견에 대한 정보를 포함하는 배열, 타입 : UsersPetType[]
  * @returns isPetPending
  * @returns petError
  *  
- * * 현재 로그인한 사용자의 userId를 사용하여 해당 사용자의 반려동물 정보를 가져오는 훅
- * * 가져온 데이터는 배열 형태로, 반려 동물이 한 마리가 아니라 여러 마리일 수도 있기 때문임.
+ * * 현재 로그인한 사용자의 userId를 사용하여 해당 사용자의 반려견 정보를 가져오는 훅
+ * * 가져온 데이터는 배열 형태로, 반려견이 한 마리가 아니라 여러 마리일 수도 있기 때문임.
  * * 사용자의 userId는 useAuthStore 훅을 이용함.
  * 
  * 사용 ) const { userPets, isPetPending, petError } = useUserPetsData();
