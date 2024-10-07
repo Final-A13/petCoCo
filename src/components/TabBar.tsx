c19845c78094573e27f5fbfbfc93990d085609e2
hyeonw8 <whgusrud223@gmail.com>
2024-10-07 22:57:22 +0900

    update: 산책 메이트 페이지에 플로팅 버튼 대신 글쓰기, 필터 버튼 추가(디자인은 좀 더 고려해볼 것)

--- src/components/TabBar.tsx
+++ src/components/TabBar.tsx
@@ -52,26 +52,41 @@ const TabBar: React.FC = () => {
   // }
 
   const isMessagePath = pathname.startsWith("/message");
+  const isMatePath = pathname.startsWith("/mate");
 
   return (
     <nav className="${isMessagePath ? 'hidden lg:flex' : 'flex'} fixed bottom-0 z-40 w-full max-w-[420px] border border-t-bgGray500 bg-white bg-opacity-80 px-2 pb-[0.7rem] pt-[0.3rem] lg:left-0 lg:top-0 lg:h-full lg:w-[180px] lg:max-w-none lg:flex-col lg:justify-start lg:border-r lg:border-t-0 lg:pt-[5rem]">
-      <div className="flex justify-between gap-x-[1.2rem] px-[0.8rem] py-[0.2rem] lg:flex-col lg:items-start lg:gap-y-[2rem]">
-        {menuItems.map((item) => (
-          <Link key={item.path} href={item.path} passHref>
-            <div className="flex flex-col items-center justify-center gap-y-[0.2rem] lg:flex-row lg:gap-x-[1rem]">
-              <Image
-                src={getImageSrc(item.path, item.defaultIcon, item.activeIcon)}
-                alt={item.label}
-                width={24}
-                height={24}
-                priority
-              />
-              <p className="text-center text-[0.61863rem] lg:text-base" style={{ color: getTextColor(item.path) }}>
-                {item.label}
-              </p>
+      <div className="lg:flex lg:h-full lg:flex-col lg:justify-between">
+        <div className="flex justify-between gap-x-[1.2rem] px-[0.8rem] py-[0.2rem] lg:flex-col lg:items-start lg:gap-y-[2rem]">
+          {menuItems.map((item) => (
+            <Link key={item.path} href={item.path} passHref>
+              <div className="flex flex-col items-center justify-center gap-y-[0.2rem] lg:flex-row lg:gap-x-[1rem]">
+                <Image
+                  src={getImageSrc(item.path, item.defaultIcon, item.activeIcon)}
+                  alt={item.label}
+                  width={24}
+                  height={24}
+                  priority
+                />
+                <p className="text-center text-[0.61863rem] lg:text-base" style={{ color: getTextColor(item.path) }}>
+                  {item.label}
+                </p>
+              </div>
+            </Link>
+          ))}
+        </div>
+        {isMatePath ? (
+          <div className="hidden lg:block lg:mb-10">
+            <div className="w-full text-center flex flex-col gap-4 text-[0.61863rem] text-base">
+              <Link href="/mate/posts" className="bg-background rounded-md p-2 cursor-pointer">
+                글쓰기
+              </Link>
+              <Link href="/mate/filter" className="bg-gray-200  rounded-md p-2 cursor-pointer">
+                필터
+              </Link>
             </div>
-          </Link>
-        ))}
+          </div>
+        ) : null}
       </div>
     </nav>
   );
