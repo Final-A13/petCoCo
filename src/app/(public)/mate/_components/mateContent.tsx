2816ded7a179a9952a9130becc45aab9496badb0
hyeonw8 <whgusrud223@gmail.com>
2024-10-22 11:48:37 +0900

    refactor: 배경 이미지 컴포넌트 분리

--- src/app/(public)/mate/_components/mateContent.tsx
+++ src/app/(public)/mate/_components/mateContent.tsx
@@ -11,6 +11,18 @@ import PlusIcon from "@/app/utils/plusIcon";
 //Type
 import { Filters } from "@/zustand/useFilterStore";
 import FloatingButton from "./filter/floatingButton";
+import Image from 'next/image';
+
+const BackgroundImage = () => (
+  <Image
+    src="/assets/svg/web_bg.svg"
+    layout="fill"
+    objectFit="cover"
+    quality={100}
+    priority
+    alt="Background"
+  />
+);
 
 const MateContent = () => {
   const [searchTerm, setSearchTerm] = useState<string>("");
@@ -61,27 +73,32 @@ const MateContent = () => {
     <div className="relative mx-auto mt-[4rem] min-h-screen max-w-[420px] lg:mt-0 lg:max-w-none">
       <div className="lg: w-full gap-y-[1.9375rem] lg:flex lg:flex-col">
         {/* 웹사이트 메인 부분 */}
-        <div className="h-full w-full lg:flex lg:flex-col lg:items-center lg:bg-gray-100 lg:bg-[url('/assets/svg/web_bg.svg')] lg:bg-cover lg:bg-center">
-          <h1 className="hidden lg:mt-[3.13rem] lg:block lg:w-[28.25rem] lg:text-[2rem] lg:font-[600]">산책 메이트</h1>
-          <p className="hidden lg:mt-[1rem] lg:block lg:w-[28.25rem] lg:text-[1rem] lg:font-[400]">
-            여러분의 소중한 반려견의 산책 친구를 만들어 주세요.
-          </p>
-          <div className="flex flex-col lg:flex lg:flex-col-reverse xl:flex xl:flex-col-reverse">
-            <div className="mt-[1rem] overflow-x-auto whitespace-nowrap scrollbar-hide lg:mb-[3.12rem] lg:ml-6 lg:mt-0">
-              <PostListFilterTab
-                handleAllPosts={handleAllPosts}
-                handleRecruiting={handleRecruiting}
-                handleDateSort={handleDateSort}
-                handleDistanceSort={handleDistanceSort}
-                handleNewSort={handleNewSort}
-                sortBy={sortBy || "all"} // 정렬 기본 값 all로 설정
-              />
-            </div>
-            <div className="mx-auto mb-[0.75rem] mt-[1.5rem] w-full px-[1.5rem] lg:mb-[1.37rem] lg:mt-[2.12rem]">
-              <SearchBar setSearchTerm={setSearchTerm} value={searchTerm} onSubmit={handleSearchPosts} />
-            </div>
-          </div>
+        <div className="h-full w-full lg:relative lg:flex lg:flex-col lg:items-center lg:bg-gray-100">
+      <div className="hidden lg:absolute lg:inset-0 lg:block">
+        <BackgroundImage />
+      </div>
+    <div className="lg:relative lg:z-10">
+      <h1 className="hidden lg:mt-[3.13rem] lg:block lg:w-[28.25rem] lg:text-[2rem] lg:font-[600]">산책 메이트</h1>
+      <p className="hidden lg:mt-[1rem] lg:block lg:w-[28.25rem] lg:text-[1rem] lg:font-[400]">
+        여러분의 소중한 반려견의 산책 친구를 만들어 주세요.
+      </p>
+      <div className="flex flex-col lg:flex lg:flex-col-reverse xl:flex xl:flex-col-reverse">
+        <div className="mt-[1rem] overflow-x-auto whitespace-nowrap scrollbar-hide lg:mb-[3.12rem] lg:ml-6 lg:mt-0">
+          <PostListFilterTab
+            handleAllPosts={handleAllPosts}
+            handleRecruiting={handleRecruiting}
+            handleDateSort={handleDateSort}
+            handleDistanceSort={handleDistanceSort}
+            handleNewSort={handleNewSort}
+            sortBy={sortBy || "all"}
+          />
         </div>
+        <div className="mx-auto mb-[0.75rem] mt-[1.5rem] w-full px-[1.5rem] lg:mb-[1.37rem] lg:mt-[2.12rem]">
+          <SearchBar setSearchTerm={setSearchTerm} value={searchTerm} onSubmit={handleSearchPosts} />
+        </div>
+      </div>
+    </div>
+  </div>
         <MatePostList activeSearchTerm={activeSearchTerm} sortBy={sortBy || "all"} filters={filters} />
       </div>
       <FloatingButton
