5cb91d39eda47626a38c690452bf6da766a91542
hyeonw8 <whgusrud223@gmail.com>
2024-11-20 12:03:21 +0900

    refactor: 배경 이미지 및 헤더 컴포넌트 분리

---
+++ src/app/(public)/mate/_components/tab/backgroundImage.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import bgImage from "/public/assets/img/mate_bg.png";
+
+export const BackgroundImage = () => (
+  <div className="hidden lg:block lg:absolute lg:inset-0">
+    <div className="relative w-full h-full"> 
+      <Image
+        src={bgImage}
+        fill
+        placeholder="blur"
+        priority={true}
+        loading="eager"
+        sizes="(max-width: 1024px) 0vw, 100vw"
+        alt="Background"
+        quality={75}
+        style={{ objectFit: 'cover' }}
+      />
+    </div>
+  </div>
+);
---
+++ src/app/(public)/mate/_components/tab/header.tsx
@@ -0,0 +1,10 @@
+export const Header = () => (
+  <div className="hidden lg:ml-6 lg:block lg:w-fit">
+    <header className="mt-[3.13rem] min-h-[1rem]"> 
+      <h1 className="text-[2rem] font-semibold leading-tight">산책 메이트</h1>
+      <h4 className="mt-4 text-base font-normal leading-relaxed">
+        여러분의 소중한 반려견의 산책 친구를 만들어 주세요.
+      </h4>
+    </header>
+  </div>
+);
